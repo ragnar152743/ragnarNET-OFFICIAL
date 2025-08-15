a79c77b0d1bdde307afa6edbf82e96ef8685e90e
ragnar152743 <botfeur10@gmail.com>
2025-08-15 18:52:18 +0200

    instal.lua

---
+++ instal.lua
@@ -0,0 +1,4 @@
+shell.run('wget run https://raw.githubusercontent.com/ragnar152743/ragnarNET-OFFICIAL/main/ragnar%3Aofficial/systeme%20FR/startup.lua')
+shell.run('wget run https://raw.githubusercontent.com/ragnar152743/ragnarNET-OFFICIAL/refs/heads/main/ragnar%3Aofficial/systeme%20FR/config.lua')
+shell.run('wget run https://raw.githubusercontent.com/ragnar152743/ragnarNET-OFFICIAL/refs/heads/main/ragnar%3Aofficial/systeme%20FR/ui.lua')
+shell.run('wget run https://raw.githubusercontent.com/ragnar152743/ragnarNET-OFFICIAL/refs/heads/main/ragnar%3Aofficial/systeme%20FR/users.db')
